96828d9610d05668ffe8eaa1a3856582dd2e2e02
Nicky Rakotoarimanana <nickyrakotoarimanana@gmail.com>
2023-10-08 20:47:27 +0200

    enigme page

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,5 @@
 const modal = document.querySelector(".modal");
-const cardContactButton = document.querySelector(".card-contact-btn");
 const modalButton = document.querySelector(".modal-btn");
-// Sélectionnez les liens du menu
-cardContactButton.addEventListener("click", function () {
-  modal.style.display = "block";
-});
 
 modalButton.addEventListener("click", function () {
   modal.style.display = "none";
